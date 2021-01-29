b94bf6a7f8b2c27925ee7addebd5e0c8e5fce51d
Sai Parimisetty MAC work station <pramodhps58@gmail.com>
2021-01-28 23:25:12 -0600

    merging readme.md

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
-    'users.apps.UsersConfig',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,5 +118,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -14,11 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from users import views as user_views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
-    path('', include('blog.urls'))
 ]
