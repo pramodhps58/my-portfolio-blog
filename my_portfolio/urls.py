6a54ae7d9ea1f86d0be7243bf98558e043602907
Sai Parimisetty MAC work station <pramodhps58@gmail.com>
2021-02-03 01:01:43 -0600

    Added User profile picture and signals to create and save profiles

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
@@ -26,3 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
